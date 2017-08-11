85f730e02dd7ffcb2b660a2ee151901f9a76d765
ocean <oceanv@163.com>
2017-08-11 09:52:38 +0800

    test log4j2 is ok

--- src/test/java/com/hai/log4j/Log4j2Test.java
+++ src/test/java/com/hai/log4j/Log4j2Test.java
@@ -1,4 +1,15 @@
 package com.hai.log4j;
 
-public class Log4j1Test {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
+public class Log4j2Test {
+    private Logger logger = LogManager.getLogger(Log4j2Test.class.getName());
+
+    /* 测试配置文件 */
+    @Test
+    public void testConfig() {
+        logger.trace("qe");
+    }
 }
